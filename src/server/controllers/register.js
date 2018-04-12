7b6f81cbf144a4c92b49494ba877f29d9b7996c1
maximus1108 <max.perry1108@yahoo.co.uk>
2018-04-12 22:11:18 +0100

    can add username to database and responds appropriately if entry already exists

--- src/server/controllers/register.js
+++ src/server/controllers/register.js
@@ -1,8 +1,31 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-module.exports = function(req, res) {
+//get register our schema and model for a user with mongoonse
+require('../models/User');
 
+module.exports = (req, res) => {
     
+    //get model for user from mongoose
+    const userModel = mongoose.model('User');
 
-    res.json({ message: `registering email: ${req.body.email}` });
+    //create new instance of user model
+    const User = new userModel();
+    
+    //store the email sent in the request on the user
+    User.email = req.body.email;
+
+    //save the user
+    User.save((err) => {
+        if(err){
+            console.log(err.error)
+        } 
+
+        //send a message and an indication of
+        //whether there was an error to the client
+        res.json({ 
+            message: err && err.message || 'Success',
+            error: err ? true : false
+        })
+    })
+    
 }
--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const User = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     email: {
         required: true,
         type: String,
@@ -18,16 +18,24 @@ const User = new mongoose.Schema({
     // hash: String
 });
 
-const UserModel = mongoose.model('User', User);
+const UserModel = mongoose.model('User', UserSchema);
 
-User.pre('save', (next) => {
-    User.findOne({ email: email }, (err, user) => {
-        if(err)
-            next(err);
-        else if(user){
-            next(new Error('User Exisits'))
+UserSchema.pre('save', function(next) {
+    UserModel.findOne({ email: this.email }, 'email', (err, user) => {
+        if(err) {
+            next({ 
+                error: err,
+                message: 'Error occured when attempting to save user'
+            });
         }
-        else
+        else if(user) {
+            next({ 
+                error: new Error('User Exists'),
+                message:  `An account is already registered with the email address '${this.email}'`
+            });
+        }
+        else {
             next();
+        }
     })
 });
