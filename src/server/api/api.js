5b42374e06734d652a63ed455381f62b56f1f00f
maximus1108 <max.perry1108@yahoo.co.uk>
2018-06-01 10:06:53 +0100

    began implementing dev specific server behaviour based on NODE_EV

--- src/server/api/api.js
+++ src/server/api/api.js
@@ -1,11 +1,41 @@
-require('dotenv').config();
-
 const express = require('express');
+const app = express();
+
+const env = process.env.NODE_ENV;
+
+//if we are developing
+if(env === 'development') {
+    //get dummy enviroment variables from .env file
+    require('dotenv').config();
+    //To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
+    app.use(function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    
+        //and remove cacheing so we get the most recent comments
+        res.setHeader('Cache-Control', 'no-cache');
+        next();
+    });
+}
+else if (env === 'production') {
+
+    const client = path.resolve('../', 'client');
+
+    app.use('/assets/js', express.static(path.resolve(client, 'assets/js')));
+    app.use('/assets/css', express.static(path.resolve(client, 'assets/css')));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(client, 'index.html'));
+    });
+}
+
+
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/index');
-const app = express();
 const passport = require('passport');
-
+const path = require('path');
 
 require('./config/db');
 
@@ -14,8 +44,7 @@ require('./config/db');
 require('./models/User');
 
 //GET CONFIG FOR PASSPORT
-require('./config/passport')
-
+require('./config/passport');
 
 var port = process.env.API_PORT || 3000;
 
@@ -23,32 +52,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  
-    //and remove cacheing so we get the most recent comments
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-  });
-
 app.use('/api', apiRouter);
-
-const path = require('path')
-
-app.use('/assets/js', express.static(path.resolve(process.cwd(), 'build/client/assets/js')));
-app.use('/assets/css', express.static(path.resolve(process.cwd(), 'build/client/assets/css')));
-
-app.get('/', (req, res) => {
-
-    const fs = require('fs')
-    const files = fs.readdirSync(__dirname);
-    console.log(files)
-    res.sendFile(path.resolve(process.cwd(), 'build', 'client', 'index.html'));
-});
 
 app.listen(port, function() {
     console.log(`api running on port ${port}`);
